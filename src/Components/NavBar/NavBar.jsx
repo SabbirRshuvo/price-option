29af5a78165ffab4112e1bedee4c4c4395d47fff
sabbirPC <sabbirhshuvo11@gmail.com>
2024-08-31 19:45:14 +0600

    navbar

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { AiOutlineMenuFold } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
+import Link from "../Link/Link";
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const routes = [
+    {
+      id: 1,
+      path: "/home",
+      name: "Home",
+    },
+    {
+      id: 2,
+      path: "/about",
+      name: "About",
+    },
+    {
+      id: 3,
+      path: "/contact",
+      name: "Contact",
+    },
+    {
+      id: 4,
+      path: "/profile/:id", // Dynamic route with parameter
+      name: "Profile",
+    },
+    {
+      id: 5,
+      path: "/settings",
+      name: "Settings",
+    },
+  ];
+
+  return (
+    <nav className="bg-yellow-300 p-6 text-black ">
+      <div className="lg:hidden text-xl " onClick={() => setOpen(!open)}>
+        {open === true ? <IoMdClose /> : <AiOutlineMenuFold />}
+      </div>
+
+      <ul
+        className={`lg:flex gap-4 absolute duration-1000
+            bg-yellow-200  mt-3 p-6
+        ${open ? "top-16" : "-top-60"}`}
+      >
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
